cdd58f81036dc3d2e81258c01436b9e02be9790f
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2020-09-11 14:26:20 +0200

    Add files via upload

---
+++ TD_Les_bases_de_python.py
@@ -0,0 +1,65 @@
+
+# exercice 1
+
+print(type("trois"))
+print(type("4"))
+print(type('5'))
+print(type("6"))
+
+# exercice 2
+
+print("il a 16 ans")
+print("Les cheveux de jean n'ont pas poussé")
+print("oui,j'arrive !")\
+                    
+# exercice 3
+
+print(20-6)
+print(2*31)
+print(len("python"))
+print(50*3/100)
+
+a=("20-6")
+b=("2*31")
+c=len("python")
+d=("50*3/100")
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# exercice 5
+
+mavariable=12
+print(mavariable)
+emeEssai=15
+print(emeEssai)
+LargeurRectangle=16
+print(LargeurRectangle)
+mavariable=17
+print(mavariable)
+ma_variable=18
+print(ma_variable)
+_ma_variable=19
+print(_ma_variable)
+MAVARIABLE=20
+print(MAVARIABLE)
+nombreéchec=21
+print(nombreéchec)
+pointdevie=22
+print(pointdevie)
+
+x=5
+print(x)
+maVariable=6
+print(maVariable)
+mot_de_passe=7
+print(mot_de_passe)
+loginUtilisateur=8
+print(loginUtilisateur)
+LongueurParcours=9
+print(LongueurParcours)
+prix_séjour=10
+print(prix_séjour)
+
