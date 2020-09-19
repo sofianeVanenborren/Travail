da7c180f364cea9f422b85684fd87a8213573af5
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2020-09-19 11:53:44 +0200

    Add files via upload

---
+++ TD nsi.py
@@ -0,0 +1,47 @@
+#exercice 8
+print(7+2/3)
+print(7+2//3)
+#question2
+print(5+2)
+print(type('5'+'2'))
+#question 3
+print(type('Python'))
+print(len('python'))
+print(type(len('python')))
+
+#exercice 9
+
+a=12
+b=a+1
+c=a
+a=a+1
+b=13
+
+toto=13.4
+toto=10.5
+toto=toto-5.5
+
+a='10'
+b='2'
+c=a+b
+
+a="4*5"
+a=a+'6'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
