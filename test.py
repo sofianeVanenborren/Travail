96940450394a05ccbb44aaeaee2289e9e70fcaa4
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2020-09-26 11:56:27 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+#cours de fonction
+help(print)
+help(hex)
+help(bin)
+help(type)
+
+#Bien faire un appel de fonction
+
+print("l'informatique, c'est cool !!")
+
+print('ma moyenne en info : ' ,18 , 'ma moyenne en français : ' , 15)
+
+
+from ExemplesFonctions import *
+
+# AFFICHAGE du résultat d'une expression
+print(3 + 5)
+
+# AFFICHAGE du résultat d'un appel de fonction
+print(racine(25))
+
+dis_bonjour()
