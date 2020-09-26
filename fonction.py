ede185841192e7f926d0ea450756825a2c17c154
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2020-09-26 11:30:36 +0200

    Add files via upload

--- fonction.py
+++ fonction.py
@@ -1,7 +1,22 @@
-m=u()
-m
-4.7
+#cours de fonction
+help(print)
+help(hex)
+help(bin)
+help(type)
 
-x=(56-4)*2
-f(x)
-true
+#Bien faire un appel de fonction
+
+dis_bonjour()
+tirage_de_6_faces()
+
+racine(25)
+print("l'informatique, c'est cool !!")
+
+est_majeur(12,4,2005)
+print('ma moyenne en info : ' ,18 , 'ma moyenne en français : ' , 15)
+
+from ExemplesFonctions import *
+
+res = 3+5
+
+res = racine(25)
