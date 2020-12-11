652cdfca2a9faddba4f928e96a38a52e93d05b90
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2020-12-11 13:51:34 +0100

    Add files via upload

--- devis.py
+++ devis.py
@@ -3,45 +3,33 @@ import math
 
 def nombre_pots(longueur, largeur, surface_couvrante):
     aire= (longueur*largeur)/2
+    return aire
     
     
 
-    pass
+    
 
 def nombre_baches(longueur, largeur, surface_couvrante):
-    """
-    Calcul du nombre de bâches nécessaires
-    paramètre longueur (float) : valeur de la longueur de la pièce
-    paramètre largeur (float) : valeur de la largeur de la pièce
-    paramètre surface_couvrante (float) ; valeur de la surface couvrante d'1 bâche
-    retour (int) : Nombre de bâches nécessaires
-    pré-conditions : la longueur, la largeur et la surface_couvrante sont des valeurs strictement positives
-    post-conditions : Néant
-    """
-    pass
+    nombre_baches = surface_couvrante/longueur*largeur
+    return nombre_baches
+   
+    
+    
 
 def nombre_rubans(longueur, largeur, perimetre_couvrante):
-    """
-    Calcul du nombre de rubans nécessaires
-    paramètre longueur (float) : valeur de la longueur de la pièce
-    paramètre largeur (float) : valeur de la largeur de la pièce
-    paramètre perimetre_couvrante (float) ; valeur du périmètre couvrant d'1 ruban
-    retour (int) : Nombre de rubans nécessaires
-    pré-conditions : la longueur, la largeur et la perimetre_couvrante sont des valeurs strictement positives
-    post-conditions : Néant
-    """
-    pass
+    rubans_necessaire = nombre_rubans/perimetre_couvrante
+    return rubans_necessaire
+    
+    
+    
 
 def salaire(longueur, largeur):
-    """
-    Calcul du salaire du peintre, sachant que le peintre couvre 1m * 1m en 1 heure pour 7,5€
-    paramètre longueur (float) : valeur de la longueur de la pièce
-    paramètre largeur (float) : valeur de la largeur de la pièce
-    retour (float) : Salaire du peintre
-    pré-conditions : la longueur, la largeur et la surface_couvrante sont des valeurs strictement positives
-    post-conditions : Néant
-    """
-    pass
+    salaire = 
+    
+    
+    
+    
+    
 
 def est_reparation_couverte(total_couts):
     """
