19dd07e2fe4479b1da51c921a1f9b49574ff54e1
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2021-01-21 18:11:03 +0100

    Add files via upload

--- geometrie.py
+++ geometrie.py
@@ -1,12 +1,12 @@
-def  aire ( longueur , largeur ):
-    aire  = ( longueur * largeur ) / 2
-    retour  aire
+def  aire ( longueur , largeur ): # On definis le nom de notre fonction qui va se nommer "aire"
+    aire  = ( longueur * largeur ) / 2  # Ceci va nous permettre de trouver la valeur grace avec "longeur" et "largeur"  
+    return  aire
 
     
-def  perimetre ( longueur , largeur ):
-    Périmètre = longueur + largeur * ( 2 )
-    retour  Périmètre
+def  perimetre ( longueur , largeur ): # On redonne un nom a cette fonction qui est "perimetre"
+    perimètre = longueur + largeur * ( 2 ) # même principe comme pour le aire on utilise les variables "longeur" et "largeur"
+    return  Perimètre
 
-si  __name__  ==  "__main__" :
+if  __name__  ==  "__main__" :
     import  doctest
     doctest . testmod ()
