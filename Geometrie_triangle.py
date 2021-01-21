bbd0c6e09b95fffec62f0345c07b65c32fd59758
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2021-01-21 17:58:02 +0100

    Add files via upload

--- Geometrie_triangle.py
+++ Geometrie_triangle.py
@@ -1,12 +1,12 @@
 # geométrie sur les triangles
-from math import
+from math import # On importe le module "math"
 
-def  aire ( base , hauteur ):
-    Aire = (base*hauteur/2)
+def  aire ( base , hauteur ): # Nom ce la fonction qui se nomme "aire"
+    Aire = (base*hauteur/2) # Utilisation de la formule de mathematique pour trouver une aire 
     return Aire
 
-def  périmètre ( base , hauteur ):
-   Périmètre = (base+hauteur)/2
+def  périmètre ( base , hauteur ): # Nom de lafonction qui se nomme "périmètre"
+   Périmètre = (base+hauteur)/2 # Re utilisation d'une formule de mathematique pour trouver un périmètre
    return Périmètre
 
 si  __name__  ==  "__main__" :
