ede905190421af975ec59d4937966aae50bb2740
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2021-02-12 14:56:29 +0100

    Add files via upload

---
+++ TD_boucle while.py
@@ -0,0 +1,49 @@
+# Exercice 1
+
+# Le code va afficher k donc 0 a l'infinie
+# Le code affichera des valeurs négatifs a l'infinis
+
+# Exercice 2
+
+i = 0
+while i < 11:
+    print(i)
+    i = i+1
+print('\n')
+    
+i = 5
+while i <= 10:
+    print(i)
+    i = i+1
+print('\n')
+
+i = 10
+while i >= 0:
+    print(i)
+    i = i-1
+print('\n')
+
+i = 0
+while i <= 10:
+    print(i)
+    i = i+2
+print('\n')
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i +2
+print('\n')    
+    
+# Exercice 3
+for i in range(11,0,-2):
+    print(i)
+# Les resultats seront impaires
+
+# Exercice 4
+
+
+ 
+        
+        
+    
