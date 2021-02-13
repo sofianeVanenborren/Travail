f1fdb2707ffd1c2c7ae7598486078bba6f71f3b5
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2021-02-13 11:56:41 +0100

    Add files via upload

---
+++ TP_Art_ASCII.py
@@ -0,0 +1,51 @@
+# Question 1 : Niveau facile
+
+"""def affiche_ligne(n):
+    for i in range(n):
+        print("O", end="")
+    return
+affiche_ligne(5)
+print()
+affiche_ligne(12)
+print('\n')"""
+
+# Question 2 : Niveau intérmédiaire
+
+"""def affiche_carre(coté):
+    for i in range(coté):
+        for k in range(coté):
+            print("O", end="")
+        print()
+affiche_carre(5)
+print()
+affiche_carre(12)"""
+
+# Question 3 : Niveau intérmédiaire
+
+"""def rectangle(hauteur,largeur):
+    for i in range(hauteur):
+        for j in range(largeur):
+            print("O",end= "")
+        print()
+rectangle(8,3)
+print()
+rectangle(5,20)"""
+
+# Question 4 : Niveau intérmédiaire
+
+"""def triangle_rectangle(n):
+    for i in range(n + 1):
+        for j in range(i):
+            print("O" , end="")
+        print()
+    return
+triangle_rectangle(5)"""
+
+
+
+   
+
+        
+
+        
+        
