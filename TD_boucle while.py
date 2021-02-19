7ff50d2ac24e076eae2d47bda03a8aa200e33421
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2021-02-19 14:50:14 +0100

    Add files via upload

--- TD_boucle while.py
+++ TD_boucle while.py
@@ -5,7 +5,7 @@
 
 # Exercice 2
 
-i = 0
+"""i = 0
 while i < 11:
     print(i)
     i = i+1
@@ -33,17 +33,53 @@ i = 1
 while i <= 10:
     print(i)
     i = i +2
-print('\n')    
+print('\n')"""   
     
 # Exercice 3
-for i in range(11,0,-2):
-    print(i)
-# Les resultats seront impaires
+"""for i in range(11,0,-2):
+    print(i)"""
+# Les resultats seront impaires si on démarre a 1
+# Lesr ésultats seront pair si on démarre a 2
 
 # Exercice 4
 
+"""def afficherEntre(nb):
+    i = -nb + 1
+    while i < nb:
+        print(i , end =" ")
+        i = i+1
+afficherEntre(5)"""
 
- 
-        
-        
+# Exercie 5
+
+"""def premier(nombre):
+    nb = 1
+    cpt = 0
+    while nb <= nombre:
+        if nombre % nb == 0:
+            cpt += 1
+        nb += 1
+    if cpt <=2:
+        return True
+    else:
+        return False
+print(premier(31))"""
+
+# Exercie 6
+
+
+
     
+    
+    
+
+            
+    
+    
+
+
+
+
+
+
+
