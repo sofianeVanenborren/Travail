e0d90c54ffc912de88e1890d3098a54ef666a1ef
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2021-05-21 14:53:31 +0200

    Add files via upload

---
+++ TD_tuples.py
@@ -0,0 +1,59 @@
+"""t = (3, 7, 2, 2, 10)
+
+t[3]
+print(t[3])
+# del(t[1]) --> une erreur
+#t(2) --> une erreur
+#(1, ) + t[4] --> une erreur
+3,4 + 5,5
+print(3,4 + 5,5)
+t[-3]
+print(t[-3])
+t[1:3]
+print(t[1:3])"""
+
+# Exercice 2
+
+"""def quatreOperation(a,b):
+    tuple = (8+2 , 8-2 , 8*2 , 8/2)
+    return tuple 
+print(quatreOperation(8,2))"""
+
+# Exercie 3
+
+"""def prefixe(a,b):
+    return (a,) +b
+nouveau_tuple = prefixe(4,(3,1,1,5,9))
+print(nouveau_tuple)
+    
+def suffixe(a,b):
+    return b + (a, )
+nouveau_tuple = suffixe(4,(3,1,1,5,9))
+print(nouveau_tuple)"""
+
+
+
+
+
+
+    
+    
+
+    
+    
+    
+
+
+
+    
+    
+
+
+    
+    
+
+
+
+    
+
+
