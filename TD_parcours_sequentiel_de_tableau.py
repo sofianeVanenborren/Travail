8bdabc634a1df8c3b4a49bf1d7a9b1f014080b16
sofianeVanenborren <70756279+sofianeVanenborren@users.noreply.github.com>
2021-05-22 11:54:57 +0200

    Add files via upload

---
+++ TD_parcours_sequentiel_de_tableau.py
@@ -0,0 +1,45 @@
+def recherche(liste, valeur):
+    if valeur in liste:
+        return True
+    else:
+        return False
+
+def recherche_indice(liste, valeur):
+    """liste.index(valeur)"""
+
+        
+def recherche_minimum(liste):
+    """
+    Description de la fonction : Renvoie la plus petite valeur de la liste
+    paramètre liste (list) : liste non vide d'entiers(int), de nombres réels(float) ou de chaîne de caractères(str)"
+    Return (int)
+    """
+    # A compléter
+
+def recherche_maximum(liste):
+    """
+    Description de la fonction : Renvoie la plus grande valeur de la liste
+    paramètre liste (list) : liste non vide d'entiers(int), de nombres réels(float) ou de chaîne de caractères(str)
+    Return (int)
+    """
+    # A compléter
+
+def moyenne(liste):
+    """
+    Description de la fonction : Calcule la moyenne des valeurs contenues dans la liste
+    paramètre liste (list) : liste non vide d'entiers(int) ou de nombres réels(float)
+    Return (int)
+    """
+    # A compléter
+    
+from random import randrange
+liste = []
+for i in range(100):
+    liste.append(randrange(500))
+
+print(liste)
+
+
+    
+    
+    
